21fce85f49bdcb36397fba7d027343186a0d06dd
Omisha <omishaiyer13@gmail.com>
2026-07-09 23:25:58 +0530

    Implement pivot index calculation in PivotIndex.cpp

---
+++ PivotIndex.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int mi = -1;
+        int sl = 0, sr = 0;
+        for(int i = 1; i<nums.size(); i++)
+        sr += nums[i];
+        for(int i = 1; i<=nums.size(); i++)
+        {
+            if(sl != sr)
+            {
+                sl += nums[i-1];
+                if(i<nums.size())
+                sr -= nums[i];
+            }
+            else
+            {
+                mi = i-1;
+                break;
+            }
+        }
+        return mi;
+    }
+};
